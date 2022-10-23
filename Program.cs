daa96384652e22e888779d5d9731335be3060b26
UlugbekMahkamov <mulugbek2309@gmail.com>
2022-10-23 15:33:23 +0300

    Main

--- Program.cs
+++ Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using kescha.Classes;
+
+
+// Console.WriteLine(" Enter you name: ");
+// string user_name = Console.ReadLine();
+// Console.WriteLine($"Hello {user_name}");
+
+// Console.WriteLine("Enter your age: ");
+// string age = Console.ReadLine();
+// int User_age = Convert.ToInt32(age);
+// Console.WriteLine("Succesfully Converted!!!!");
+
+// int Kescha_age = 3;
+// int Difference_age = User_age - Kescha_age;
+// Console.WriteLine($"Your age and KEscha's age difference {Difference_age}");
+
+
+// if(User_age> Kescha_age)
+// {
+//     Console.WriteLine("You are older");
+// }
+// else if(User_age< Kescha_age)
+// {
+//     Console.WriteLine("KEscha is older");
+// }
+// else
+// {
+//     Console.WriteLine("You and KEscha equal");
+// }
+
+
+// Console.WriteLine("Let me Tell you about my friend:  ");
+
+// int Eshmat_age = 1;
+// int Toshmat_age = 2;
+
+// Console.WriteLine($"Eshmat is {Eshmat_age} years old");
+// Console.WriteLine($"Toshmat is {Toshmat_age} years old");
+// Console.WriteLine($"{user_name} is {User_age} years old");
+
